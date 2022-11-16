8c8802aa71e12fa2475b31cddfaeb39cd8871ebc
Nirajan Malla <nirajannitro5@gmail.com>
2022-11-16 17:29:52 +0545

    just adding fun text and all

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
 import Login from './components/login'
+import Logout from './components/logout'
 import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
   const { user, isAuthenticated, isLoading } = useAuth0();
   return (
-    <div className='holder'>
-      <h1>Hi beautiful soul, signup or login to know the truth of life XD</h1>
-      <Login />
+    <>
+      <div>
+        {!isAuthenticated && <h1>Homepage</h1>} <nav>let us consider this is a navbar</nav>
+      </div>
+      <div className='holder'>
+
+        <h1>Hi beautiful soul, signup or login to know the truth of life XD</h1>
+        {isAuthenticated ? <Logout /> : <Login />}
+      </div>;
       <marquee width="30%" direction="left" height="100px">
-        this is footer 🐱‍👤 @ninjaNirajan
+        and this is a footer 🐱‍👤 @ninjaNirajan
       </marquee>
-    </div>
+    </>
   );
 }
 
